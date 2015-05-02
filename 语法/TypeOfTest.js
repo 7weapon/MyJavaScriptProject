51dd41955ed1016c63b5cdee795a5ad20818e591
HawkingChan <Chan597463138@gmail.com>
2015-05-02 16:39:22 +0800

    闭包

---
+++ 语法/TypeOfTest.js
@@ -0,0 +1,15 @@
+/**
+ * Created by HawkingChan on 15/5/2.
+ */
+var t1 = typeof ("111");
+console.log(t1);//string类型
+var t2 = typeof (1);
+console.log(t2);//number类型
+var t3 = typeof (true);
+console.log(t3);//boolean类型
+var t4 = typeof (undefined);
+console.log(t4);//undefined类型
+var t5 = typeof (null);
+console.log(t5);//object类型
+var result = NaN == NaN;
+console.log(result);
---
+++ 语法/闭包.js
@@ -0,0 +1,38 @@
+/**
+ * Created by HawkingChan on 15/5/2.
+ */
+var result = [];
+//function foo()
+//{
+//    for(var i = 0; i < 5; i++)
+//    {
+//        result[i] = function () {
+//            console.log(i);
+//        };
+//    }
+//}
+//foo();
+//result[0]();
+//result[4]();
+/*这种写法希望foo函数变量i被内部循环的函数使用，并且分别能获得他们的索引，但是实际上只能获得该变量最后保留的值
+  也就是说，闭包中记录的自由变量，只是对这个变量的一个引用，而非变量的值，当这个变量被改变了，闭包获取的变量值
+  也会被改变
+ */
+
+//解决方法
+function foo()
+{
+    for(var i = 0;i < 5;i++)
+    {
+        result[i] = (function (j) {
+            //console.log(j);
+            return function()
+            {
+                console.log(j);
+            }
+        })(i);
+    }
+}
+foo();
+result[0]();
+result[1]();
