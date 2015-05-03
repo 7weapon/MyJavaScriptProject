8a397f0a4b667945c6db9a9b3d4e99f994ee6043
HawkingChan <hpuchenkai@live.cn>
2015-05-03 21:55:20 +0800

    面向对象编程

---
+++ 语法/oopTest1.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Fluky on 2015/5/3.
+ */
+(function(){
+     function Person(name,sex)
+     {
+         this._name = name;
+         this._sex = sex;
+     }
+     Person.prototype = {
+          say:function(){
+              alert(this._name+'  '+this._sex);
+          }
+     };
+    //将类的接口开放
+    window.Person = Person;
+}());
+
+(function () {
+    function Student(name,sex)
+    {
+        this._name = name;
+        this._sex = sex;
+    }
+    //student继承Person
+    Student.prototype = new Person();
+    var superSay = Student.prototype.say;
+    //对say方法重写
+    Student.prototype.say = function(){
+        superSay.call(this);
+        alert('override');
+    };
+    window.Student = Student;
+}());
---
+++ 语法/oopTest2.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Fluky on 2015/5/3.
+ */
+(function(){
+   function Person(name)
+   {
+       var _this ={};
+       _this.name = name;
+       _this.say = function()
+       {
+         alert('P.Name:'+_this.name)
+       };
+       return _this;
+   }
+    window.Person = Person;
+}());
+
+(function(){
+    function Student(name)
+    {
+        var _this = Person(name);
+        //Person中的方法复用
+        var superSay = _this.say;
+        _this.say = function(){
+            superSay.call(_this);
+          alert('S.name:'+_this.name);
+        };
+        return _this;
+    }
+    window.Student = Student;
+}());
