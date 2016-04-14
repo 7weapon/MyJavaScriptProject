08c07661a427de40763d5221a45f7c043a3ff291
HawkingChan <Chan597463138@gmail.com>
2016-04-14 10:08:25 +0800

    es6 Object扩展方法

---
+++ es6/Object/extend2.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chenkai3 on 2016/4/12.
+ */
+'use strict';
+
+const user = {
+    name: 'kk',
+    age: 26,
+    phone: '18515659527'
+}
+
+const {name,age} = user;
+
+console.log(name, age);
