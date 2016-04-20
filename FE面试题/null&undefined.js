730cf26da788554190fdc614201ef2070bf58d19
HawkingChan <Chan597463138@gmail.com>
2016-04-20 10:28:54 +0800

    null && undefined

--- FE面试题/null&undefined.js
+++ FE面试题/null&undefined.js
@@ -2,7 +2,24 @@
  * Created by chenkai3 on 2016/4/20.
  */
 
+/*
 console.log(typeof null);  //没有对象
 console.log(null instanceof Object);
 
 console.log(typeof undefined);//没有值
+*/
+
+
+var obj = {};
+
+console.log(obj.name);     //对象不存在的属性 undefined
+
+var name ;
+function test()
+{
+    "use strict";
+
+    this.name = 'kk';
+}
+
+console.log(test.name)
