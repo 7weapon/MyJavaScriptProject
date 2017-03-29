701088532e0a9456d29bb3b010fb1af17f4a1198
陈凯 <chenkai@zuoyebang.com>
2017-03-29 19:32:53 +0800

    使用js 控制 动画

--- interviewQuestions/utility.js
+++ interviewQuestions/utility.js
@@ -8,55 +8,55 @@
  * @returns {*}
  */
 function getFuncName(func) {
-    if (typeof func !== 'function') {
-        throw  new TypeError('support Type Function');
-    }
-    if (func.name) {
-        return func.name
-    }
-    var ret = func.toString();
-    ret = ret.substr('function '.length);
-    ret = ret.substr(0, ret.indexOf('C'));
-    return ret;
+  if (typeof func !== 'function') {
+    throw  new TypeError('support Type Function');
+  }
+  if (func.name) {
+    return func.name
+  }
+  var ret = func.toString();
+  ret = ret.substr('function '.length);
+  ret = ret.substr(0, ret.indexOf('C'));
+  return ret;
 
 }
 
 
 if (!Function.prototype.bind) {
-    Function.prototype.bind = function (oThis) {
-        if (typeof  this !== 'function') {
-            throw  new TypeError('Function.prototype.bind-what is trying to be bound is not callable');
-        }
-
-        //获取参数数组
-        var aArgs = Array.prototype.slice.call(arguments, 1);
-        var fToBind = this;   //被绑定的函数对象
-        var fNOP = function () {
-        };
-        var fBound = function () {
-            return fToBind.apply(this instanceof fNOP ? this : oThis || this, aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-
-        return fNOP;
+  Function.prototype.bind = function (oThis) {
+    if (typeof  this !== 'function') {
+      throw  new TypeError('Function.prototype.bind-what is trying to be bound is not callable');
     }
+
+    //获取参数数组
+    var aArgs = Array.prototype.slice.call(arguments, 1);
+    var fToBind = this;   //被绑定的函数对象
+    var fNOP = function () {
+    };
+    var fBound = function () {
+      return fToBind.apply(this instanceof fNOP ? this : oThis || this, aArgs.concat(Array.prototype.slice.call(arguments)));
+    };
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fNOP;
+  }
 }
 
 function getQueryObject(url) {
-    "use strict";
-    url = url || window.location.href;
-    var search = url.substr(url.lastIndexOf('?'));
-    var obj = {};
-    var reg = /([^?&=]+)=([^?&=]*)/g;
-    search.replace(reg, function (match, $1, $2) {
-        var name = decodeURIComponent($1);
-        var value = decodeURIComponent($2);
-        value = String(value);
-        obj[name] = value;
-    });
+  "use strict";
+  url = url || window.location.href;
+  var search = url.substr(url.lastIndexOf('?'));
+  var obj = {};
+  var reg = /([^?&=]+)=([^?&=]*)/g;
+  search.replace(reg, function (match, $1, $2) {
+    var name = decodeURIComponent($1);
+    var value = decodeURIComponent($2);
+    value = String(value);
+    obj[name] = value;
+  });
 
-    return obj;
+  return obj;
 }
 
 /**
@@ -65,45 +65,70 @@ function getQueryObject(url) {
  * @param context
  */
 function throttle(method, context) {
-    "use strict";
-    clearTimeout(method.tId);
-    method.tId = setTimeout(()=> {
-        method.call(context);
-    }, 100)
+  "use strict";
+  clearTimeout(method.tId);
+  method.tId = setTimeout(() => {
+    method.call(context);
+  }, 100)
 }
 
 var throttleV1 = function (fn, delay) {
-    "use strict";
-    var timer = null;
-    return ()=> {
-        var context = this, args = arguments;
-        clearTimeout(timer);
-        timer = setTimeout(()=> {
-            fn.apply(context, args);
-        }, delay);
-    }
+  "use strict";
+  var timer = null;
+  return () => {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, delay);
+  }
 };
 
 var throttleV2 = function (fn, dely, mustRunDely) {
-    "use strict";
-    var timer = null;
-    var t_start;
-    return ()=> {
-        var context = this, args = arguments, t_cur = +new Date();  //获取毫秒数
-        clearTimeout(timer);
-        if (!t_start) {
-            t_start = t_cur;
-        }
-        if (t_cur - t_start >= mustRunDely) {
-            fn.apply(context, args);
-            t_start = t_cur;
-        }
-        else {
-            timer = setTimeout(()=> {
-                fn.apply(context, args);
-            }, dely)
-        }
+  "use strict";
+  var timer = null;
+  var t_start;
+  return () => {
+    var context = this, args = arguments, t_cur = +new Date();  //获取毫秒数
+    clearTimeout(timer);
+    if (!t_start) {
+      t_start = t_cur;
     }
+    if (t_cur - t_start >= mustRunDely) {
+      fn.apply(context, args);
+      t_start = t_cur;
+    }
+    else {
+      timer = setTimeout(() => {
+        fn.apply(context, args);
+      }, dely)
+    }
+  }
 };
 
+function inherits(subClass, superClass) {
+  var F = new Function();
+  F.prototype = superClass.prototype;
+  subClass.prototype = new F();
+  subClass.prototype.constructor = subClass;
+}
 
+
+function Dog() {
+}
+Dog.prototype = {
+  constructor: Dog,
+  alert: function () {
+    console.log(this.name);
+  }
+}
+
+function Cat(){
+
+}
+
+inherits(Cat,Dog);
+
+var cat = new Cat();
+cat.name = 'cat';
+cat.alert();
