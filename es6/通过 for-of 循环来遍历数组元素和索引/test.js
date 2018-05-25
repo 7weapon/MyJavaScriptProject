640f67e5976f97ea7b2aad8232c70b6a197212cf
chenkai <chenkai22@meituan.com>
2018-05-25 18:00:03 +0800

    forin forof 区别

--- es6/通过 for-of 循环来遍历数组元素和索引/test.js
+++ es6/通过 for-of 循环来遍历数组元素和索引/test.js
@@ -7,8 +7,16 @@
  */
 "use strict";
 
+Array.prototype.func1 = function(){};
+Object.prototype.func2 = function(){};
+
 const arr = ['a', 'b', 'c', 'd'];
+arr.kk = 'kk';
+
+for(const k in arr){
+    console.log(k);
+}
 
 for (const [index,el] of arr.entries()) {
-    console.log(`index = ${index},el = ${el}`);
+    console.log(`${el}`);
 }
